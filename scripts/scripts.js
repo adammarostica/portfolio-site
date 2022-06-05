bbb1db97085cfed05f753822f7162bea6c1e5093
adammarostica <10792897+adammarostica@users.noreply.github.com>
2022-06-05 17:10:45 -0400

    Reverting to old grow keyframes for Safari test

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,24 @@
-function debounce(func, wait = 10, immediate = true) {
-  let timeout;
-  return function (...args) {
-    const context = this;
-    const later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    const callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-}
+// function debounce(func, wait = 10, immediate = true) {
+//   let timeout;
+//   return function (...args) {
+//     const context = this;
+//     const later = function () {
+//       timeout = null;
+//       if (!immediate) func.apply(context, args);
+//     };
+//     const callNow = immediate && !timeout;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(later, wait);
+//     if (callNow) func.apply(context, args);
+//   };
+// }
 
-function checkScroll () {
-  document.documentElement.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-  console.log(document.documentElement.style);
-}
+// function checkScroll () {
+//   document.documentElement.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+//   console.log(document.documentElement.style);
+// }
 
-window.addEventListener('scroll', debounce(checkScroll), false);
+// window.addEventListener('scroll', debounce(checkScroll), false);
 
 // setTimeout(function() {
 //   const thing = document.querySelector('.intro__name__A1');
