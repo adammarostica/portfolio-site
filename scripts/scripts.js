c3dc3ff41e11f5c1a22d7b690bb6a31f6aea44ae
adammarostica <10792897+adammarostica@users.noreply.github.com>
2022-06-05 22:55:26 -0400

    Fussin with the about section, position sticky and event scrolling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,26 @@
-// function debounce(func, wait = 10, immediate = true) {
-//   let timeout;
-//   return function (...args) {
-//     const context = this;
-//     const later = function () {
-//       timeout = null;
-//       if (!immediate) func.apply(context, args);
-//     };
-//     const callNow = immediate && !timeout;
-//     clearTimeout(timeout);
-//     timeout = setTimeout(later, wait);
-//     if (callNow) func.apply(context, args);
-//   };
-// }
+function debounce(func, wait = 10, immediate = true) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
-// function checkScroll () {
-//   document.documentElement.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-//   console.log(document.documentElement.style);
-// }
-
-// window.addEventListener('scroll', debounce(checkScroll), false);
+const about = document.getElementById('about');
+function checkScroll () {
+  console.log(about.pageYOffset, about.offsetHeight, about.innerHeight);
+  document.documentElement.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  console.log(document.documentElement.style);
+}
+console.log(about);
+window.addEventListener('scroll', debounce(checkScroll), false);
 
 // setTimeout(function() {
 //   const thing = document.querySelector('.intro__name__A1');
